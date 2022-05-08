338f0b80073597ca57c423c03c7a81db57cfc9ef
Juan Morales <jgamesterror@gmail.com>
2022-05-07 23:23:19 -0500

    falta twitter

--- types/types.js
+++ types/types.js
@@ -3,7 +3,6 @@ const types = {
   baseUrl: process.env.BASE_URL,
   forgotPassword: process.env.FORGOT_PASSWORD,
   verifiedEmail: process.env.VERIFIED_EMAIL,
-  defaultPassword: process.env.DEFAULT_PASSWORD,
   googleIdClient: process.env.GOOGLE_ID_CLIENT,
   sengridApiKey: process.env.SENDGRID_API_KEY,
   defaultEmail: process.env.DEFAULT_EMAIL,
